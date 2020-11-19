a93ae32eae1b82fbdb44b0c0cdef6fdb9c7a25b7
Maanav Rao <58347499+NPUZenith@users.noreply.github.com>
2020-11-19 18:10:07 -0500

    Part 2

--- src/Wheel.java
+++ src/Wheel.java
@@ -4,6 +4,10 @@ public class Wheel {
 
     double radius;
 
+    /*
+     Allows the new wheel to pass in a variable named "radius".
+     The second line is object oriented stuff that tells all of the other functions outside of main to use "radius" when running the commands.
+    */
     Wheel(double radius)
     {
         this.radius = radius;
@@ -15,8 +19,20 @@ public class Wheel {
         Scanner s = new Scanner(System.in);
         double d = s.nextDouble();
 
-        System.out.println(d);
+        Wheel w = new Wheel(d); // Sends the scannered double "d" into the wheel constructor up top to count that as radius
 
-        Wheel w = new Wheel(d);
+
+        System.out.println("You circumference is " + w.getCircumference());
+        System.out.println("You area is " + w.getArea());
+    }
+
+    double getCircumference()
+    {
+        return 2 * Math.PI * radius;
+    }
+
+    double getArea()
+    {
+        return Math.sqrt(radius) * Math.PI;
     }
 }
